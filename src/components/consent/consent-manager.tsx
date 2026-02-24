f31dffe5781b1bf1f3c92f945e3c47d0b5acae3d
Fouad <fouadezari@gmail.com>
2026-02-24 07:56:02 +0100

    Feat: Update ConsentManager to include cookie usage description

--- src/components/consent/consent-manager.tsx
+++ src/components/consent/consent-manager.tsx
@@ -3,6 +3,28 @@ import { ConsentManagerDialog, ConsentManagerProvider, CookieBanner } from '@c15
 import type { ReactNode } from 'react'
 
 export default function ConsentManager({ children }: { children: ReactNode }) {
+  const description = (
+    <>
+      <p>
+        {`Le site du RPDAD utilise des cookies pour améliorer votre expérience et comprendre comment vous l’utilisez. En les autorisant, vous acceptez le dépôt et la lecture de cookies et l'utilisation de technologies de suivi nécessaires à leur bon fonctionnement.`}
+      </p>
+      <p>
+        {`Nous conservons vos choix pendant une durée de 30 jours. Vous pouvez changer d'avis à tout moment en cliquant sur le bouton ci-dessous.`}
+      </p>
+      <p>
+        {`Pour plus d’informations et exercer vos droits, nous vous recommandons de consulter notre `}
+        <a
+          href="https://www.rpdad.fr/mentions-legales"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold underline hover:opacity-80"
+        >
+          politique de confidentialité
+        </a>
+        {`.`}
+      </p>
+    </>
+  )
   return (
     <ConsentManagerProvider
       options={{
@@ -19,7 +41,7 @@ export default function ConsentManager({ children }: { children: ReactNode }) {
               cookieBanner: {
                 title: 'Utilisation de cookies',
                 description:
-                  'Nous utilisons des cookies pour vous offrir la meilleure expérience possible. En continuant à utiliser notre site, vous acceptez notre utilisation de cookies.',
+                  "Le site du RPDAD de la Gironde utilise des cookies pour finalité d’assurer le bon fonctionnement du site, comprendre comment vous l’utilisez, interagir avec les réseaux sociaux. En les autorisant, vous acceptez le dépôt et la lecture de cookies et l'utilisation de technologies de suivi nécessaires à leur bon fonctionnement.",
               },
               consentManagerDialog: {
                 title: 'Réglages des cookies',
@@ -50,7 +72,7 @@ export default function ConsentManager({ children }: { children: ReactNode }) {
         ignoreGeoLocation: true, // Useful for development to always view the banner.
       }}
     >
-      <CookieBanner />
+      <CookieBanner description={description} />
       <ConsentManagerDialog />
       {children}
     </ConsentManagerProvider>
